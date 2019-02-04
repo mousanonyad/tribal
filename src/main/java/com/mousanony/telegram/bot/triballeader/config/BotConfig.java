d2503e7453e7a600737b87345adbf8e06cb1ec49
sbt-denisov-an <ANDenisov.SBT@sberbank.ru>
2019-02-04 17:08:15 +0300

    init commit

---
+++ src/main/java/com/mousanony/telegram/bot/triballeader/config/BotConfig.java
@@ -0,0 +1,14 @@
+package com.mousanony.telegram.bot.triballeader.config;
+
+/**
+ * @author mousanonyad
+ */
+public class BotConfig {
+
+    private BotConfig() {
+    }
+
+    public static final String BOT_NAME = "WebHook";
+    public static final String BOT_TOKEN = "token";
+    public static final String BOT_URL = "t.me/TribalLeaderBot";
+}
---
+++ src/main/java/com/mousanony/telegram/bot/triballeader/handlers/LongPolling.java
@@ -0,0 +1,45 @@
+package com.mousanony.telegram.bot.triballeader.handlers;
+
+import com.mousanony.telegram.bot.triballeader.config.BotConfig;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+/**
+ * @author mousanonyad
+ */
+public class LongPolling extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BotConfig.BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConfig.BOT_TOKEN;
+    }
+
+}
