88ae6d3075d84fef11ff58ab184d0e6649c8fce9
sbt-denisov-an <ANDenisov.SBT@sberbank.ru>
2019-02-04 19:57:15 +0300

    Прототип 1

---
+++ src/main/java/com/mousanony/telegram/bot/triballeader/TelegramRespond.java
@@ -0,0 +1,13 @@
+package com.mousanony.telegram.bot.triballeader;
+
+import com.mousanonyad.messaging.IRespond;
+
+/**
+ * @author mousanonyad
+ */
+public class TelegramRespond implements IRespond {
+    @Override
+    public void output(String s) {
+
+    }
+}
--- src/main/java/com/mousanony/telegram/bot/triballeader/handlers/LongPolling.java
+++ src/main/java/com/mousanony/telegram/bot/triballeader/handlers/LongPolling.java
@@ -1,23 +1,42 @@
 package com.mousanony.telegram.bot.triballeader.handlers;
 
+import com.mousanony.telegram.bot.triballeader.TelegramRespond;
 import com.mousanony.telegram.bot.triballeader.config.BotConfig;
+import com.mousanonyad.session.GameSession;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author mousanonyad
  */
 public class LongPolling extends TelegramLongPollingBot {
 
+    private Map<Long, GameSession> gameSessionMap;
+
     @Override
     public void onUpdateReceived(Update update) {
+        GameSession gameSession;
         if (update.hasMessage() && update.getMessage().hasText()) {
+
+            Message updateMessage = update.getMessage();
+            Long chatId = updateMessage.getChatId();
+            if (gameSessionMap.containsKey(chatId)) {
+                gameSession = gameSessionMap.get(chatId);
+            } else {
+                gameSession = new GameSession(30, new TelegramRespond());
+                gameSessionMap.put(chatId, gameSession);
+            }
+
+
+
             SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
+                    .setChatId(chatId)
                     .setText(update.getMessage().getText());
             try {
                 execute(message); // Call method to send the message
